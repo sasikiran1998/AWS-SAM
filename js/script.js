65a13fc18ae59afa828d4d9adacef39aab54befa
sasi <69857268+sasikiran1998@users.noreply.github.com>
2021-07-16 13:47:18 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,87 +1,278 @@
-const statusDisplay = document.querySelector('.game--status');
+var modal = document.getElementById('myModal');
 
-let gameActive = true;
-let currentPlayer = "X";
-let gameState = ["", "", "", "", "", "", "", "", ""];
 
-const winningMessage = () => `Player ${currentPlayer} has won!`;
-const drawMessage = () => `Game ended in a draw!`;
-const currentPlayerTurn = () => `It's ${currentPlayer}'s turn`;
+var grid = new Array(3);
+grid[0] = new Array(3);
+grid[1] = new Array(3);
+grid[2] = new Array(3);
+var player = 1;
+var gameWon = 0;
 
-statusDisplay.innerHTML = currentPlayerTurn();
-
-const winningConditions = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-];
-
-function handleCellPlayed(clickedCell, clickedCellIndex) {
-    gameState[clickedCellIndex] = currentPlayer;
-    clickedCell.innerHTML = currentPlayer;
-}
-
-function handlePlayerChange() {
-    currentPlayer = currentPlayer === "X" ? "O" : "X";
-    statusDisplay.innerHTML = currentPlayerTurn();
-}
-
-function handleResultValidation() {
-    let roundWon = false;
-    for (let i = 0; i <= 7; i++) {
-        const winCondition = winningConditions[i];
-        let a = gameState[winCondition[0]];
-        let b = gameState[winCondition[1]];
-        let c = gameState[winCondition[2]];
-        if (a === '' || b === '' || c === '') {
-            continue;
-        }
-        if (a === b && b === c) {
-            roundWon = true;
-            break
-        }
+$("#square_one").click(function() {
+  console.log("clicked");
+  if (checkLegalMove(0, 0) == true) {
+    if (player == 1) {
+      $("#square_one_text").html("X");
+      grid[0][0] = 'X';
+      if (checkWin(1) == true) {
+        endgame(1);
+      }
+      player = 2;
+    } else {
+      $("#square_one_text").html("O");
+      grid[0][0] = 'O';
+      if (checkWin(2) == true) {
+        endgame(2);
+      }
+      player = 1;
     }
+  }
+});
 
-    if (roundWon) {
-        statusDisplay.innerHTML = winningMessage();
-        gameActive = false;
-        return;
+$("#square_two").click(function() {
+  if (checkLegalMove(0, 1) == true) {
+    if (player == 1) {
+      $("#square_two_text").html("X");
+      grid[0][1] = 'X';
+      if (checkWin(1) == true) {
+        endgame(1);
+      }
+      player = 2;
+    } else {
+      $("#square_two_text").html("O");
+      grid[0][1] = 'O';
+      if (checkWin(2) == true) {
+        endgame(2);
+      }
+      player = 1;
     }
+  }
+});
 
-    let roundDraw = !gameState.includes("");
-    if (roundDraw) {
-        statusDisplay.innerHTML = drawMessage();
-        gameActive = false;
-        return;
+$("#square_three").click(function() {
+  if (checkLegalMove(0, 2) == true) {
+    if (player == 1) {
+      $("#square_three_text").html("X");
+      grid[0][2] = 'X';
+      if (checkWin(1) == true) {
+        endgame(1);
+      }
+      player = 2;
+    } else {
+      $("#square_three_text").html("O");
+      grid[0][2] = 'O';
+      if (checkWin(2) == true) {
+        endgame(2);
+      }
+      player = 1;
     }
+  }
+});
 
-    handlePlayerChange();
+$("#square_four").click(function() {
+  if (checkLegalMove(1, 0) == true) {
+    if (player == 1) {
+      $("#square_four_text").html("X");
+      grid[1][0] = 'X';
+      if (checkWin(1) == true) {
+        endgame(1);
+      }
+      player = 2;
+    } else {
+      $("#square_four_text").html("O");
+      grid[1][0] = 'O';
+      if (checkWin(2) == true) {
+        endgame(2);
+      }
+      player = 1;
+    }
+  }
+});
+
+$("#square_five").click(function() {
+  if (checkLegalMove(1, 1) == true) {
+    if (player == 1) {
+      $("#square_five_text").html("X");
+      grid[1][1] = 'X';
+      if (checkWin(1) == true) {
+        endgame(1);
+      }
+      player = 2;
+    } else {
+      $("#square_five_text").html("O");
+      grid[1][1] = 'O';
+      if (checkWin(2) == true) {
+        endgame(2);
+      }
+      player = 1;
+    }
+  }
+});
+
+$("#square_six").click(function() {
+  if (checkLegalMove(1, 2) == true) {
+    if (player == 1) {
+      $("#square_six_text").html("X");
+      grid[1][2] = 'X';
+      if (checkWin(1) == true) {
+        endgame(1);
+      }
+      player = 2;
+    } else {
+      $("#square_six_text").html("O");
+      grid[1][2] = 'O';
+      if (checkWin(2) == true) {
+        endgame(2);
+      }
+      player = 1;
+    }
+  }
+});
+
+$("#square_seven").click(function() {
+  if (checkLegalMove(2, 0) == true) {
+    if (player == 1) {
+      $("#square_seven_text").html("X");
+   
+            grid[2][0] = 'X';
+      if (checkWin(1) == true) {
+        endgame(1);
+      }
+   player = 2;
+    } else {
+      $("#square_seven_text").html("O");
+      grid[2][0] = 'O';
+      if (checkWin(2) == true) {
+        endgame(2);
+      }
+      player = 1;
+    }
+  }
+});
+
+$("#square_eight").click(function() {
+  if (checkLegalMove(2, 1) == true) {
+    if (player == 1) {
+      $("#square_eight_text").html("X");
+      grid[2][1] = 'X';
+      if (checkWin(1) == true) {
+        endgame(1);
+      }
+      player = 2;
+    } else {
+      $("#square_eight_text").html("O");
+      grid[2][1] = 'O';
+      if (checkWin(2) == true) {
+        endgame(2);
+      }
+      player = 1;
+    }
+  }
+});
+
+$("#square_nine").click(function() {
+  if (checkLegalMove(2, 2) == true) {
+    if (player == 1) {
+      $("#square_nine_text").html("X");
+      grid[2][2] = 'X';
+      if (checkWin(1) == true) {
+        endgame(1);
+      }
+      player = 2;
+    } else {
+      $("#square_nine_text").html("O");
+      grid[2][2] = 'O';
+      if (checkWin(2) == true) {
+        endgame(2);
+      }
+      player = 1;
+    }
+  }
+});
+
+function checkWin(playerNum) {
+  //check horizontal
+  for (i = 0; i < 3; i++) {
+
+    if ((grid[i][0] == grid[i][1] && grid[i][1] == grid[i][2]) &&  grid[i][0] != undefined && grid[i][1] != undefined && grid[i][2] != undefined) {
+     console.log("horizontal won");
+      return true;
+    }
+  }
+
+  //check vertical
+  for (i = 0; i < 3; i++) {
+    console.log("i is: " + i);
+    console.log("grid[" + i + "][0] is " + grid[i][0]);
+    console.log("grid[" + i + "][1] is " + grid[i][1]);
+    console.log("grid[" + i + "][2] is " + grid[i][2]);
+    if ((grid[0][i] == grid[1][i] && grid[1][i] == grid[2][i]) && grid[0][i] != undefined && grid[1][i] != undefined && grid[2][i] != undefined) {
+      console.log("vertical won");
+      return true;
+    }
+  }
+
+  //check diagonal
+  if (((grid[0][0] == grid[1][1] && grid[1][1] == grid[2][2]) || (grid[0][2] == grid[1][1] && grid[1][1] == grid[2][0])) && grid[1][1] !== undefined) {
+     console.log("diagonal won");
+    return true;
+  }
+
+  var tieGame = true;
+  for (var i = 0; i < 3; i++) {
+    for (var x = 0; x < 3; x++) {
+      if (grid[i][x] == null && grid[i][x] == undefined) {
+        tieGame = false;
+      } 
+    }
+  }
+
+  if (tieGame == true) {
+    endgame(0);
+  }
+
+  return false;
 }
 
-function handleCellClick(clickedCellEvent) {
-    const clickedCell = clickedCellEvent.target;
-    const clickedCellIndex = parseInt(clickedCell.getAttribute('data-cell-index'));
-
-    if (gameState[clickedCellIndex] !== "" || !gameActive) {
-        return;
-    }
-
-    handleCellPlayed(clickedCell, clickedCellIndex);
-    handleResultValidation();
+function checkLegalMove(row, column) {
+  console.log(grid[row][column]);
+  if (grid[row][column] !== undefined && grid[row][column] !== null) {
+    return false;
+  } else {
+    return true;
+  }
 }
 
-function handleRestartGame() {
-    gameActive = true;
-    currentPlayer = "X";
-    gameState = ["", "", "", "", "", "", "", "", ""];
-    statusDisplay.innerHTML = currentPlayerTurn();
-    document.querySelectorAll('.cell').forEach(cell => cell.innerHTML = "");
+function endgame(num) {
+  if (num == 0) {
+    $(".modal_text").html("Tie game!");
+    $("#myModal").css("display", "block");
+  }
+  if (num == 1) {
+    $(".modal_text").html("Congratulations!! Player 1 Wins!");
+    $("#myModal").css("display", "block");
+  }
+  if (num == 2) {
+    $(".modal_text").html("Congratulations!! Player 2 Wins!");
+    $("#myModal").css("display", "block");
+  }
 }
 
-document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', handleCellClick));
-document.querySelector('.game--restart').addEventListener('click', handleRestartGame);
+$("#restartBtn").click(function(){
+    grid = new Array(3);
+    grid[0] = new Array(3);
+    grid[1] = new Array(3);
+    grid[2] = new Array(3);
+    player = 1;
+    gameWon = 0;
+    $("#square_one_text").html("");
+    $("#square_two_text").html("");
+    $("#square_three_text").html("");
+    $("#square_four_text").html("");
+    $("#square_five_text").html("");
+    $("#square_six_text").html("");
+    $("#square_seven_text").html("");
+    $("#square_eight_text").html("");
+    $("#square_nine_text").html("");
+    modal.style.display = "none";
+});
